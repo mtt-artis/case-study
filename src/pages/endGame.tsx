a89d4b4a27e1c769dfaaf3c27917ade39dddce92
mtt <>
2023-04-01 13:35:51 +0200

    fix

--- src/pages/endGame.tsx
+++ src/pages/endGame.tsx
@@ -1,13 +1,13 @@
-import { createSignal, For, Show } from 'solid-js';
+import { For, Show } from 'solid-js';
 import { useCards } from '../context';
 
 const base = import.meta.env.BASE_URL;
 
 export const EndGame = () => {
-  const [{ cards, actionNumber }] = useCards();
+  const [{ cards, errorCount }] = useCards();
   return (
     <section class="grid gap-16 p-8" >
-      <div class="my-4">{actionNumber()}</div>
+      <div class="my-4">{errorCount()}</div>
       <For each={cards.filter(c => c.showInPosition).sort((a, b) => a.showInPosition > b.showInPosition ? 1 : -1)}>
         {(card) =>
           <div class="grid gap-4 grid-cols-2">
